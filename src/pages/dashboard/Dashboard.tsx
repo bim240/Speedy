fb11f220b228c33dede359f9fd776dd993494ef1
bim240 <bimlendu357@gmail.com>
2024-11-10 15:37:03 +0530

    performance improvement

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import classes from './styles.module.css'
 import KeyMetric from './components/key-metric/KeyMetric'
-import UserGrowth from './components/user-growth'
-import RevenueDistribution from './components/revenue-distribution'
-import TopSongs from './components/top-songs'
-import SongTable from './components/song-table/SongTable'
+import Skeleton from '../../component/skeleton'
+// import UserGrowth from
+// import RevenueDistribution from './components/revenue-distribution'
+// import TopSongs from './components/top-songs'
+// import SongTable from './components/song-table/SongTable'
 
+const UserGrowth = React.lazy(
+  () => import(/*webpackChunkName: "user-growth" */ './components/user-growth'),
+)
+const RevenueDistribution = React.lazy(
+  () => import(/*webpackChunkName: "revenue-distribution" */ './components/revenue-distribution'),
+)
+const TopSongs = React.lazy(
+  () => import(/*webpackChunkName: "top-songs" */ './components/top-songs'),
+)
+const SongTable = React.lazy(
+  () => import(/*webpackChunkName: "song-table" */ './components/song-table/SongTable'),
+)
 export default function Dashboard() {
   return (
     <>
@@ -14,13 +27,21 @@ export default function Dashboard() {
       </div>
       <div className={classes.box2}>
         <div className={classes.box3}>
-          <UserGrowth />
-          <RevenueDistribution />
+          <React.Suspense fallback={<Skeleton />}>
+            <UserGrowth />
+          </React.Suspense>
+          <React.Suspense fallback={<Skeleton />}>
+            <RevenueDistribution />
+          </React.Suspense>
         </div>
-        <TopSongs />
+        <React.Suspense fallback={<Skeleton />}>
+          <TopSongs />
+        </React.Suspense>
       </div>
       <div className={classes.tableBox}>
-        <SongTable />
+        <React.Suspense fallback={<Skeleton />}>
+          <SongTable />
+        </React.Suspense>
       </div>
     </>
   )
